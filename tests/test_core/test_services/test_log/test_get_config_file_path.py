00688a5a17c668aa352c3518d66dd2634282d2bc
robin <leikt.solreihin@gmail.com>
2024-01-27 19:00:18 +0100

    fix tests

--- tests/test_core/test_services/test_log/test_get_config_file_path.py
+++ tests/test_core/test_services/test_log/test_get_config_file_path.py
@@ -1,58 +1,68 @@
 import os
 
 import pytest
-import tempfile
 
 from waster.core.services.log import _get_config_file_path, LOGGING_CFG_ENVVAR, LOGGING_CFG_DEFAULT  # NOQA
 
 
+def _create_temp_file(filename: str) -> str:
+    path = f'./{filename}'
+    with open(path, 'w') as file:
+        file.write('{}')
+
+    return path
+
+
+def _init():
+    if LOGGING_CFG_ENVVAR in os.environ:
+        os.environ.pop(LOGGING_CFG_ENVVAR)
+
+
 def test_get_config_file_path___w_file_wo_envvar():
     # With a file but no environment variable
-    temp = tempfile.TemporaryFile()
-    result = _get_config_file_path(str(temp.name))
-    assert result == temp.name
+    _init()
+    path = _create_temp_file('test_get_config_file_path___w_file_wo_envvar.json')
+    assert _get_config_file_path(path) == path
 
 
 def test_get_config_file_path___w_file_w_envvar():
     # With environment variable and a file
+    _init()
+    path = _create_temp_file('test_get_config_file_path___w_file_w_envvar.json')
     os.environ[LOGGING_CFG_ENVVAR] = 'foo.json'
-    temp = tempfile.TemporaryFile()
-    result = _get_config_file_path(str(temp.name))
-    assert result == temp.name
+    assert _get_config_file_path(path) == path
 
 
 def test_get_config_file_path___wo_file_w_envvar():
     # With environment variable but without file
-    temp = tempfile.TemporaryFile()
-    os.environ[LOGGING_CFG_ENVVAR] = str(temp.name)
-    result = _get_config_file_path()
-    assert result == temp.name
+    _init()
+    path = _create_temp_file('test_get_config_file_path___wo_file_w_envvar.json')
+    os.environ[LOGGING_CFG_ENVVAR] = path
+    assert _get_config_file_path() == path
 
 
 def test_get_config_file_path___bad_file():
     # Provided file does not exist
+    _init()
     with pytest.raises(FileNotFoundError):
         _get_config_file_path('foo.json')
 
 
+def test_get_config_file_path___default():
+    # No file and no environment variable
+    _init()
+    if not os.path.isdir('config'):
+        os.mkdir('config')
+    if not os.path.isfile('config/logging.json'):
+        with open('config/logging.json', 'w') as file:
+            file.write('{}')
+
+    assert _get_config_file_path() == LOGGING_CFG_DEFAULT
+
+
 def test_get_config_file_path___bad_envvar():
     # Environment variable file does not exist
+    _init()
     os.environ[LOGGING_CFG_ENVVAR] = 'foo.json'
     with pytest.raises(FileNotFoundError):
         _get_config_file_path()
-
-
-def test_get_config_file_path___default():
-    # No file and no environment variable
-    os.environ.pop(LOGGING_CFG_ENVVAR)
-    if not os.path.isdir(os.path.dirname(LOGGING_CFG_DEFAULT)):
-        os.mkdir(os.path.dirname(LOGGING_CFG_DEFAULT))
-
-    with open(LOGGING_CFG_DEFAULT, 'w') as file:
-        file.write('{}')
-
-        result = _get_config_file_path()
-        assert result == LOGGING_CFG_DEFAULT
-
-    os.remove(LOGGING_CFG_DEFAULT)
-    os.rmdir(os.path.dirname(LOGGING_CFG_DEFAULT))
